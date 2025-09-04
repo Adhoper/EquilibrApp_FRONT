bdfb40b630e2396a1efb2d720e3681b99800022f
Adrian Micael Curet Ortiz <112102886+Adhoper@users.noreply.github.com>
2025-09-04 18:42:34 -0400

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        //loadComponent: () => import("./autenticacion/login/login.component").then(c => c.LoginComponent)
+    },
+    // {
+    //     path: '',
+    //     loadComponent: () => import("./shared/components/layout/layout.component").then(c => c.LayoutComponent),
+    //     canActivate:[AuthGuard],
+    //     children: [
+    //         {
+    //         path: 'dashboard',
+    //         loadComponent: () => import("./pages/dashboard/dashboard.component").then(c => c.DashboardComponent),
+    //         },
+    //         ...laboratoriosRoutes,
+    //         ...aulasRoutes,
+    //         ...usuarioRoutes,
+    //         ...historialExperimentoRoutes,
+    //         {
+    //             path: '**',
+    //             redirectTo: 'dashboard',
+    //         },
+
+    // ]
+    // },
+];
